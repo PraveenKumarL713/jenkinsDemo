8180b10138f200f7d0347313a8d715c6dce64799
praveenkumarpraveen713 <praveenkumarpraveen713@gmail.com>
2022-02-22 13:31:09 +0530

    modifying calcultor.java adn calcexec.java

--- CalcExec.java
+++ CalcExec.java
@@ -5,6 +5,7 @@ public class CalcExec {
 		Calculator c = new Calculator();
 		System.out.println("Sum of 20 and 10 is : " + c.add(20, 10));
 		System.out.println("Difference of 20 and 10 is : " + c.sub(20, 10));
+		System.out.println("Product of 20 and 10 is : " + c.mul(20, 10));
 
 	}
 
