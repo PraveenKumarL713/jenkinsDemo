2c696fe7ae0704ba60ad99d2f7fe7cb0ac71cd88
praveenkumarpraveen713 <praveenkumarpraveen713@gmail.com>
2022-02-22 13:17:52 +0530

    commiting calcultor.java adn calcexec.java

---
+++ CalcExec.java
@@ -0,0 +1,11 @@
+public class CalcExec {
+
+	public static void main(String[] args) {
+		
+		Calculator c = new Calculator();
+		System.out.println("Sum of 20 and 10 is : " + c.add(20, 10));
+		System.out.println("Difference of 20 and 10 is : " + c.sub(20, 10));
+
+	}
+
+}
